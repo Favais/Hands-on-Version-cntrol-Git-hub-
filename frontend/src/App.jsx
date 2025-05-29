4fe4554d59f6000a4bcaa3ce9e816b5170d5dbad
Aisosa Oghagbon <oghagbonfav@gmail.com>
2025-05-29 11:41:01 +0100

    Update App.jsx to render dashboard

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,10 +10,10 @@ function App() {
   return (
     <div className='h-screen '>
       <Routes>
-        <Route path='/' element={<Home />} />
+        <Route path='/' element={<Dashboard />} />
         <Route path='/login' element={<Login />} />
         <Route path='/register' element={<Register />} />
-        <Route path='/dashboard/*' element={<Dashboard />} />
+{/*         <Route path='/dashboard/*' element={<Dashboard />} /> */}
       </Routes>
     </div>
   )
